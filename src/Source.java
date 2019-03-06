b5eb044d9534d37bc60c2c18145ecd244672af8a
user <user@DESKTOP-0JE3UDE>
2019-03-06 18:44:45 +0900

    연습장과 소스연습장 추가

---
+++ src/Source.java
@@ -0,0 +1,8 @@
+
+public class Source {
+	public static void main(String[] args) {
+		System.out.println("재미있는 JAVA");
+		System.out.println("잘하고 싶은 JAVA");
+	}
+
+}
---
+++ src/연습장.java
@@ -0,0 +1,8 @@
+
+public class 연습장 {
+	public static void main (String[] args) {
+		System.out.println("이것은 연습장 입니다");
+		System.out.println("나는 컴퓨터를 잘하고 싶습니다");
+	}
+	
+}
